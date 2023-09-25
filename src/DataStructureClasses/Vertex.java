f6a245b51af0d854bb5f4d2757b5235eaf2e1617
eros <villarroel.eros@correo.unimet.edu.ve>
2023-09-25 14:27:48 -0700

    Cambiar codigo a ingles

--- src/DataStructureClasses/Vertex.java
+++ src/DataStructureClasses/Vertex.java
@@ -1,13 +1,61 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package DataStructureClasses;
 
 /**
  *
- * @author B-St
+ * @author andre
  */
 public class Vertex {
+    private String name;
+    private int numVertex;
+    
+    public Vertex(String name){
+        this.name = name;
+        this.numVertex = -1;
+    }
+    
+    // saber si dos vertices son iguales
+    public boolean equality(Vertex vertice){
+        return this.name.equals(vertice.getName());
+    }
+    
+    // establecer el numero de vertices
+    public void assignVertex(int num){
+        setNumVertex(num);
+    }
+    
+    // caracteristicas del vertice
+    public String toString(){
+        return this.name + " (" + this.numVertex + ")";
+    }
+    
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the numVertex
+     */
+    public int getNumVertex() {
+        return numVertex;
+    }
+
+    /**
+     * @param numVertex the numVertex to set
+     */
+    public void setNumVertex(int numVertex) {
+        this.numVertex = numVertex;
+    }
+    
     
 }
---
+++ src/FileReader/Reader.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package FileReader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author andre
+ */
+public class Reader {
+
+    public void createTXT() {
+        File directory = new File(".");
+        File file = new File(directory, "archivos.txt");
+        
+        try {
+            if (file.createNewFile()) {
+                JOptionPane.showMessageDialog(null, "Archivo creado con exito!");
+            }
+        } catch (IOException excepcion) {
+            excepcion.printStackTrace(System.out);
+        }
+        // cerrar archivo?
+    }
+
+    public void writeToTXT() {
+        try {
+            File directory = new File(".");
+            File file = new File(directory, "archivos.txt");
+            FileWriter write = new FileWriter(file);
+            
+            write.write("Saludos bra");
+            write.write("\nHows going man");
+            write.write("\nWould you ever take the opportunity?");
+            
+            write.close();
+        } catch (IOException excepcion) {
+            excepcion.printStackTrace(System.out);
+        }
+    }
+
+    public void readFromTXT() {
+        //lista:
+        
+        String line;
+        
+        try {
+            //File dir = new File("C:\\Archivo");
+            File directory = new File(".");
+            File file = new File(directory, "usuarios.txt");
+            FileReader filereader = new FileReader(file);
+            BufferedReader reader = new BufferedReader(filereader);
+            
+            while((line = reader.readLine()) != null){
+                System.out.println(line);
+                line = reader.readLine();
+            }
+            reader.close();
+
+        } catch (IOException excepcion) {
+            excepcion.printStackTrace(System.out);
+        }
+    }
+
+}
