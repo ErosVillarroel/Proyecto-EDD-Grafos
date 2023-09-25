bd76090dac961b5c90e3ba0382d6dc6a642c4ab4
aemr-LL7 <a.marquez2020@correo.unimet.edu.ve>
2023-09-25 13:34:16 -0400

    Commit de prueba archivo main

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Holla mundo");
+        System.out.println("Wenas its me man");
     
     }
     
