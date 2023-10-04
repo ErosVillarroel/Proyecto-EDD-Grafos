f6b391ec027c81635b6924717a7bb6273d88389f
eros <villarroel.eros@correo.unimet.edu.ve>
2023-10-04 11:26:41 -0700

    No c q hice

--- src/Classes/Graph.java
+++ src/Classes/Graph.java
@@ -28,6 +28,7 @@ public class Graph {
     }
     
     public void print(){
+        
         for (int i = 0; i<matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 System.out.print(matrix[i][j] + " ");
