da3a0151a1c02047cb3baf615abff4151310f316
eros <villarroel.eros@correo.unimet.edu.ve>
2023-10-16 12:04:46 -0700

    Lunes 16-10

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,17 +9,13 @@ public class Main {
     public static void main(String[] args) {
         
         /*
-        -Construir Clases de datos
+
         -Disenar interfaz grafica:
-        
-            -Disenar el panel
-            -Anadir menu de opciones
             -Graficar el grafo
         
         -Construir logica:
         
             -Logica para verificar la existencia de archivos
-            -Logica para ingresar nuevos archivos
             -Logica para modificar archivos 
             -Parsear la data a leible
             -
@@ -28,7 +24,7 @@ public class Main {
         */
         
         MainInterface interfaz1 = new MainInterface();
-//        FileManager fileManager = new FileManager();
+
         
     }
     
